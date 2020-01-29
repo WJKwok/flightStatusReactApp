dd7afae2d9f500ded307de2c1b9bddd3b43a3c79
Wen <kwok.wenjian@gmail.com>
2020-01-29 16:36:12 +0100

    flight api call

--- src/Components/SearchPage/SearchPage.js
+++ src/Components/SearchPage/SearchPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import { FlightCard } from '../FlightCard/FlightCard';
 
@@ -15,15 +16,30 @@ class SearchPage extends Component {
         console.log(this.state.flightNo);
     }
 
+    componentDidMount() {
+        const appId = '8a3f5b9c';
+        const appKey = '97ed09e2c8b1052afeaae034e602802f';
+        const carrier = 'tp';
+        const flight = '536';
+        const date = '2020/1/29';
+        let url = `/flex/flightstatus/rest/v2/json/flight/status/${carrier}/${flight}/arr/${date}?appId=${appId}&appKey=${appKey}&utc=false`
+        axios.get(url)
+        .then((resp) => {
+            console.dir(resp);
+        });
+    }
+
     render() {
         return(
             <div className='white-overlay'>
                 <label>Flight Number</label>
-                <input id="flightNo"
-                    placeholder="e.g. FR1647" 
-                    value={this.state.flightNo}
-                    onChange={this.changeHandler}/>
-                
+                <div className='h-align'>
+                    <input id="flightNo"
+                        placeholder="e.g. FR1647" 
+                        value={this.state.flightNo}
+                        onChange={this.changeHandler}/>
+                    <a className="waves-effect waves-light btn">Search</a>
+                </div>
                 <FlightCard/>
             </div>
         );
